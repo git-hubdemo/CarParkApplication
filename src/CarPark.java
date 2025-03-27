2d68760a1ecb2ed776ea27dd90f3b7f99c67c175
Jayvibeee <jeffery.aideyan-ohonba@dvla.gov.uk>
2025-03-27 21:04:42 +0000

    Updated method calls to take arguments

--- src/CarPark.java
+++ src/CarPark.java
@@ -36,12 +36,12 @@ public class CarPark {
         if(carAtEntrance.equalsIgnoreCase("yes")){
             String vehicleIdentifier; //used to store either number plate or barcode
 
-            entrySensor.detectCar();
+            entrySensor.detectCar("entrance");
 
             // Check if there is space in the car park first before trying to let them in
             if(fullSign.isLightOn()){
                 System.out.println("The car park is full! entry denied");
-                entrySensor.reset(); //turn entry sensor off
+                entrySensor.reset("entrance"); //turn entry sensor off
             }
             else{
                 // Check if the car park uses a barcode reader so that a barcode can be generated for the user
@@ -56,11 +56,11 @@ public class CarPark {
 
 
                 if(parkedCars.add(vehicleIdentifier)){ // This does a check for duplicates and adds the car at the same time if no duplicates are found (HashSet returns true or false)
-                    entryBarrier.raise();
+                    entryBarrier.raise("Entrance");
                     decrementSpaces();
-                    entrySensor.reset();
-                    entryBarrier.lower();
-                    System.out.println("Car parked! Remaining spaces: " + spacesLeft());
+                    System.out.println("Car with id " + vehicleIdentifier + " successfully parked! Remaining spaces: " + spacesLeft());
+                    entryBarrier.lower("Entrance");
+                    entrySensor.reset("entrance");
                 }else{
                     System.out.printf("There is already a car with the id - %s parked!\n", vehicleIdentifier);
                 }
@@ -71,19 +71,19 @@ public class CarPark {
         if(carAtExit.equalsIgnoreCase("yes")){
             String vehicleIdentifier;
 
-            exitSensor.detectCar();
+            exitSensor.detectCar("exit");
 
             System.out.println((idReader instanceof BarcodeReader) ? "Enter your barcode to exit:" : "Enter your number plate to exit:"); // asks for the right id based on the car park's id reader type
-            vehicleIdentifier = idReader.readId();
+            vehicleIdentifier = idReader.readId(); //readId() validates the id first before returning it
 
             if(parkedCars.remove(vehicleIdentifier)){ // This does a check to see if the vehicle id is found
-                exitBarrier.raise();
+                exitBarrier.raise("Exit");
                 incrementSpaces();
-                exitSensor.reset();
-                exitBarrier.lower();
-                System.out.printf("Car with id %s exited. Remaining spaces: %d", vehicleIdentifier, spacesLeft());
+                System.out.println("Car with id " + vehicleIdentifier + " successfully exited the car park! Remaining spaces: " + spacesLeft());
+                exitBarrier.lower("Exit");
+                exitSensor.reset("exit");
             }else{
-                System.out.printf("Exit denied. The car with the id - %s is not in the parking lot.", vehicleIdentifier);
+                System.out.printf("Exit denied. The car with the id - %s is not in the car park.\n", vehicleIdentifier);
             }
         }
 
