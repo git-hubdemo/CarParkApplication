2a39edcd747f2517fa6d4c89c9c1da302d090c82
Jayvibeee <jeffery.aideyan-ohonba@dvla.gov.uk>
2025-03-30 22:45:33 +0100

    Changed IDReader class to a generic class + removed unwanted attribute + added an abstract method for displaying entrance or exit message

--- src/IDReader.java
+++ src/IDReader.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public abstract class IDReader {
-    protected String serialNumber;
+public abstract class IDReader<T> {
     protected Scanner scanner;
 
     IDReader() {
-        Random random = new Random();
-        this.serialNumber = "A" + (100000 + random.nextInt(900000));
         this.scanner = new Scanner(System.in);
     }
 
-    abstract String readId();
+
+    abstract T readId();
+
+    abstract void displayEntranceOrExitMessage(String location);
 }
