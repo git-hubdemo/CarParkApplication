d2d096ffde21aca76f0d1906f7456286b832af24
Jayvibeee <jeffery.aideyan-ohonba@dvla.gov.uk>
2025-03-30 23:00:57 +0100

    Removed redundant check for asking if the user will like to continue the simulation

--- src/CarParkSystem.java
+++ src/CarParkSystem.java
@@ -20,28 +20,24 @@ public class CarParkSystem {
 
         // Check what type of id reader is used in the chosen car park so that the user can be informed
         if(chosenCarPark.getIdReader() instanceof BarcodeReader){
-            System.out.println("The car park you chose uses a barcode system. A barcode will be generated for you.");
+            System.out.println("The car park you chose uses a barcode system. Your vehicle ID will be generated for you.");
         }
 
+        System.out.println();
         System.out.println("**********Simulation Starts**********");
         // Start the simulation loop.
-        for(int i = 0; i < 10; i++){
+        int simulationIterations = 10;
+        for(int i = 0; i < simulationIterations; i++){
             chosenCarPark.manageOperations();
-            System.out.println("Iteration " + (i + 1) + " of 10: polling car park components and reacting to state");
+            System.out.println("Iteration " + (i + 1) + " of " + simulationIterations + ": polling car park components and reacting to state");
 
             // Ask the user if they want to continue only when they are not on the second to last iteration
-            String continueChoice = InputValidator.getYesOrNoInput("Do you want to continue the simulation? (yes/no):");
-            if (continueChoice.equalsIgnoreCase("no")) {
-                System.out.println("Simulation ended.");
-                break; // Exit the loop
-            }
-
-            try {
-                System.out.println("Press Enter to continue to the next car park operation...");
-                System.in.read();
-            }
-            catch (Exception e) {
-                System.out.println("An error occurred while waiting for input. Please try again.");
+            if(i != simulationIterations - 1){
+                String continueChoice = InputValidator.getYesOrNoInput("Do you want to continue to the next car park operation? (yes/no):");
+                if (continueChoice.equalsIgnoreCase("no")) {
+                    System.out.println("Simulation ended.");
+                    break; // Exit the loop
+                }
             }
         }
     }
@@ -64,6 +60,5 @@ public class CarParkSystem {
 
             System.out.println("Invalid option. Try again.");
         }
-
     }
 }
